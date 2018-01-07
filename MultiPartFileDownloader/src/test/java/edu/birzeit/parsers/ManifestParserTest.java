412eee6a2845245871e8473755e661fbc853bfd4
Ahd Radwan <radwanahd@gmail.com>
2018-01-07 02:33:18 +0200

    Add test cases for ManifestParser and StringUtils.

---
+++ MultiPartFileDownloader/src/test/java/edu/birzeit/parsers/ManifestParserTest.java
@@ -0,0 +1,66 @@
+/**
+ * 
+ */
+package edu.birzeit.parsers;
+
+import static org.junit.Assert.*;
+
+import java.util.Map;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import com.sun.net.httpserver.Authenticator.Success;
+
+import edu.birzeit.exceptions.ManiFestParserException;
+import edu.birzeit.structures.Segment;
+
+/**
+ * Test edu.birzeit.parsers.ManifestParser
+ * @author AhdRadwan
+ *
+ */
+public class ManifestParserTest {
+	ManifestParser parser = new ManifestParser();
+	/**
+	 * Test method for {@link edu.birzeit.parsers.ManifestParser#parseManifestURLContent(java.lang.String)}.
+	 */
+	@Test
+	public void testParseManifestURLContentWithValidContent() {
+		String fileContent = ""
+				+ "http://machine1.birzeit.edu/picture.jpg-segment1\n"
+				+ "http://machine2.birzeit.edu/picture.jpg-segment1\n"
+				+ "**\n"
+				+ "http://machine1.birzeit.edu/picture.jpg-segment2\n"
+				+ "http://machine2.birzeit.edu/picture.jpg-segment2\n"
+				+ "**\n"
+				+ "http://machine1.birzeit.edu/picture.jpg-segment3\n"
+				+ "http://machine2.birzeit.edu/picture.jpg-segment3\n"
+				+ "";
+	
+		try {
+			Map<String, Segment> segmants =	parser.parseManifestURLContent(fileContent);
+			Assert.assertEquals("Provided a valid file content and the expexted number of sengements is 3", segmants.size(), 3 );
+
+			
+		} catch (ManiFestParserException e) {
+			e.printStackTrace();
+			fail();
+		}
+		
+
+	}
+	
+	/**
+	 * Test {@link edu.birzeit.parsers.ManifestParser#parseManifestURLContent(java.lang.String)}.
+	 * with invalid segments file content.
+	 * And expecting a ManiFestParserException.
+	 *
+	 */
+	@Test  (expected = ManiFestParserException.class)
+	public void testParseManifestURLContentWithnValidContent() throws ManiFestParserException {
+		String fileContent = "invalid segments";
+	
+			Map<String, Segment> segmants =	parser.parseManifestURLContent(fileContent);
+	}
+}
---
+++ MultiPartFileDownloader/src/test/java/edu/birzeit/utils/StringUtilsTest.java
@@ -0,0 +1,47 @@
+package edu.birzeit.utils;
+
+import static org.junit.Assert.*;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * Test the string Utils.
+ * 
+ * @author AhdRadwan
+ *
+ */
+public class StringUtilsTest {
+
+	@Test
+	public void testIsEmpty() {
+		boolean result = StringUtils.isEmpty("");
+		Assert.assertTrue("Provided string should be empty and  result should be true", result);
+	}
+	
+	@Test
+	public void testIsEmptyWithNonEmptyString() {
+		boolean result = StringUtils.isEmpty("This is a string");
+		Assert.assertFalse("Provided non empty string and  result should be false", false);
+	}
+
+
+	@Test
+	public void testIsNotEmpty() {
+		boolean result = StringUtils.isNotEmpty("Non Empty String");
+		Assert.assertTrue("Provided non empty string and  result should be true", result);
+	}
+
+	@Test
+	public void testIsBlank() {
+		boolean result = StringUtils.isBlank("     ");
+		Assert.assertTrue("Provided a blank string and result should be true", result);		
+	}
+
+	@Test
+	public void testIsNotBlank() {
+		boolean result = StringUtils.isNotBlank("This is a string");
+		Assert.assertTrue("Provided a not blank string and result should be true", result);		
+	}
+
+}
